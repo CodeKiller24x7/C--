11e41be71ada9271571e655dff2035ae3af06d88
CodeKiller24x7 <codekiller24x7@gmail.com>
2022-08-09 00:08:00 +0530

    first commit

---
+++ Assignment_1/Ass1_06.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   int a,b,temp;
+   cout<<"Enter Two Number : ";
+   cin>>a>>b;
+   cout<<"Before Swap : "<<endl;
+   cout<<"a = "<<a<<endl<<"b = "<<b<<endl;
+
+   temp=a;
+   a=b;
+   b=temp;
+
+   cout<<"After Swap : "<<endl;
+   cout<<"a = "<<a<<endl<<"b = "<<b<<endl;
+
+   return 0;
+}
